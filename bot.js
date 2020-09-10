09d89def9b49726a7c530c2c27a2eb3af33d9c5d
Enes <enessolak99@gmail.com>
2020-09-10 17:18:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,11 +13,11 @@ app.use(express.static('public'))
 app.use(express.urlencoded({
     extended: true
 }))
-app.use(basicAuth({
-    users: {
-        'meneksefamily': 'sertacabimbe'
-    }
-}))
+// app.use(basicAuth({
+    // users: {
+        // 'meneksefamily': 'sertacabimbe'
+    // }
+// }))
 
 app.get('/', function (req, res) {
     res.sendFile('index.html')
